e32dcbca427d0cd79f84700e699b850dc7572f3c
Raktim Kalita <rktmkalita2@gmail.com>
2022-11-06 21:55:16 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (89.80%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -2,17 +2,11 @@ class Solution {
     public int[] plusOne(int[] digits) {
         int n=digits.length;
         int carry=1;
-        // digits[n-1]++;
-        // if(digits[n-1]<=9){
-        //     return digits;
-        // }
-        // carry=digits[n-1]/10;
-        // digits[n-1]%=10;
         for(int i=n-1;i>=0;i--){
             if(carry!=0){
-                digits[i]=digits[i]+carry;
+                digits[i]+=carry;
                 carry=digits[i]/10;
-                digits[i]=digits[i]%10;
+                digits[i]%=10;
             }else{
                 return digits;
             }
