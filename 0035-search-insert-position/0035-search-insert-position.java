b37246dde0844894f39c7592dd23062bf9c7a50f
Raktim Kalita <rktmkalita2@gmail.com>
2022-11-06 21:08:49 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (63.11%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,6 +1,7 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
         int n=nums.length;
+        int left=0,right=n-1;
         
         if(nums[0]>=target){
             return 0;
@@ -9,11 +10,17 @@ class Solution {
             return n;
         }
         
-        for(int i=1;i<n;i++){
-            if(nums[i]>=target){
-                return i;
+        while(left<=right){
+            int mid=(left+right)/2;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]>target){
+                right=mid-1;
+            }else{
+                left=mid+1;
             }
         }
-        return n;
+        
+        return right+1;
     }
 }
