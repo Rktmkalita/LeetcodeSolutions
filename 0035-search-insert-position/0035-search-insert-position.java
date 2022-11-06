e11e315fedf01701b737cefd0393e36708dfac3d
Raktim Kalita <rktmkalita2@gmail.com>
2022-11-06 20:45:18 +0530

    Time: 1 ms (20.69%), Space: 43.8 MB (23.17%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,10 +1,5 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        // int i=0;
-        // while(i<nums.length && nums[i]<=target){
-        //     i++;
-        // }
-        // return i-1;
         for(int i=0;i<nums.length;i++){
             if(nums[i]==target){
                 return i;
