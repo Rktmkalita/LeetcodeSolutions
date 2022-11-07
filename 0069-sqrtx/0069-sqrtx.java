65a121f5d02a94921a5c54fe7465c60a33518d99
Raktim Kalita <rktmkalita2@gmail.com>
2022-11-08 00:21:50 +0530

    Time: 3 ms (54.17%), Space: 41.3 MB (57.44%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,13 +1,5 @@
 class Solution {
     public int mySqrt(int x) {
-        // int i=0;
-        // return i-1;
-        // while(i*i<=x){
-        //     if(i*i==x)
-        //         return i;
-        //     i++;
-        // }
-        // return i-1;
         if(x<2){
             return x;
         }
