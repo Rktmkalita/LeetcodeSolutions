d21af5f978722eced399e68a4fadb247fbbfafa2
Raktim Kalita <rktmkalita2@gmail.com>
2022-11-18 20:27:28 +0530

    Time: 1 ms (76.47%), Space: 44.3 MB (6.16%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -14,29 +14,29 @@
  * }
  */
 class Solution {
-    public boolean hasPathSum(TreeNode root, int targetSum) {
-        return traverseSum(root,targetSum,0);
+     public boolean hasPathSum(TreeNode root, int targetSum) {
+        return traverseSum(root,targetSum);
     }
     
-    boolean traverseSum(TreeNode root, int targetSum, int sum) {
+    boolean traverseSum(TreeNode root, int targetSum) {
         if(root==null){
             return false;
         }
         
-        sum+=root.val;
+        targetSum-=root.val;
         
-        if(root.left==null && root.right==null && sum==targetSum){
+        if(root.left==null && root.right==null && targetSum==0){
             return true;
         }
         
-        if(root.left==null && sum<targetSum){
-            return traverseSum(root.right,targetSum,sum);
+        if(root.left==null && targetSum!=0){
+            return traverseSum(root.right,targetSum);
         }
         
-        if(root.right==null && sum<targetSum){
-            return traverseSum(root.left,targetSum,sum);
+        if(root.right==null && targetSum!=0){
+            return traverseSum(root.left,targetSum);
         }
         
-        return traverseSum(root.right,targetSum,sum) || traverseSum(root.left,targetSum,sum);
+        return traverseSum(root.right,targetSum) || traverseSum(root.left,targetSum);
     }
 }
