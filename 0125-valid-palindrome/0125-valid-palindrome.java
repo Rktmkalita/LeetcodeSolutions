67025e61c6dd3f345e533eb44923374d66bf8d8a
Raktim Kalita <rktmkalita2@gmail.com>
2022-11-28 21:49:35 +0530

    Time: 4 ms (92.73%), Space: 42.6 MB (91.12%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,33 +1,5 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        // char[] ch = s.toLowerCase().toCharArray();
-        // int i=0,j=s.length()-1;
-        // while(true){
-        //     if(!(Character.isDigit(ch[i]) || (ch[i]>='a' && ch[i]<='z'))){
-        //         i++;
-        //         if(i>=j){
-        //             System.out.println("returning false");
-        //             return false;
-        //         }
-        //         System.out.println("1 "+ch[i]+" "+ch[j]);
-        //     }else if(!(Character.isDigit(ch[i]) || (ch[j]>='a' && ch[j]<='z'))){
-        //         j--;
-        //         if(i>=j){
-        //             System.out.println("returning false");
-        //             return false;
-        //         }
-        //         System.out.println("2 "+ch[i]+" "+ch[j]);
-        //     }else{
-        //         if(ch[i]!=ch[j]){
-        //             System.out.println("false "+ch[i]+" "+ch[j]);
-        //             return false;
-        //         }else{
-        //             System.out.println("true "+ch[i]+" "+ch[j]);
-        //             i++;
-        //             j--;
-        //         }
-        //     }
-        // }
         s=s.toLowerCase();
         char[] ch = new char[s.length()];
         int j=0;
@@ -37,16 +9,12 @@ class Solution {
                 j++;
             }
         }
-        // String str = String.copyValueOf(ch);
-        // System.out.println(str);
         int i=0;
         j--;
         while(i<j){
             if(ch[i]!=ch[j]){
-                // System.out.println("false "+ch[i]+" "+ch[j]);
                 return false;
             }else{
-                // System.out.println("true "+ch[i]+" "+ch[j]);
                 i++;
                 j--;
             }
