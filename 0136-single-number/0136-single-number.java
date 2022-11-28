527c979b76219d376f66a7d74894aaeff587625c
Raktim Kalita <rktmkalita2@gmail.com>
2022-11-28 22:08:37 +0530

    Time: 21 ms (29.79%), Space: 50.7 MB (43.38%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,9 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int n=nums.length;
-        int xor=0;
-        for(int i=0;i<n;i++)
-            xor=xor^nums[i];
-        return xor;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i+=2) {
+            if (nums[i] != nums[i + 1]) return nums[i];
+        }
+        return nums[nums.length - 1];  
     }
 }
