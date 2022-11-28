1166682bcba884530b124fda5d25a7b7282c3b07
Raktim Kalita <rktmkalita2@gmail.com>
2022-11-28 22:07:54 +0530

    Time: 6 ms (60.91%), Space: 50.4 MB (60.64%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,16 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int sum=0;
-        Set<Integer> set = new HashSet<Integer>();
-        for(int i:nums){
-            if(!set.contains(i)){
-                set.add(i);
-                sum+=i;
-            }else{
-                set.remove(i);
-                sum-=i;
-            }
-        }
-        return sum;
+        int n=nums.length;
+        int xor=0;
+        for(int i=0;i<n;i++)
+            xor=xor^nums[i];
+        return xor;
     }
 }
