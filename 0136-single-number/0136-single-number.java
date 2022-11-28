7f932e3ac280509aaf716aff301771f9146801e1
Raktim Kalita <rktmkalita2@gmail.com>
2022-11-28 21:58:32 +0530

    Time: 15 ms (33.56%), Space: 54.2 MB (12.99%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int sum=0;
+        Set<Integer> set = new HashSet<Integer>();
+        for(int i:nums){
+            if(!set.contains(i)){
+                set.add(i);
+                sum+=i;
+            }else{
+                sum-=i;
+            }
+        }
+        return sum;
+    }
+}
