5a53a86708d9c1ffe1a99ad2b39bd033853654ac
Raktim Kalita <rktmkalita2@gmail.com>
2022-12-21 21:04:34 +0530

    Time: 13 ms (42.43%), Space: 55.9 MB (48.59%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,18 +1,6 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Map<Integer,Integer> map = new HashMap<>();
-        int maxVal=0,maxKey=0;
-        for(int i:nums){
-            if(map.containsKey(i)){
-                map.put(i,map.get(i)+1);
-            }else{
-                map.put(i,1);
-            }
-            if(map.get(i)>maxVal){
-                maxKey=i;
-                maxVal=map.get(i);
-            }
-        }
-        return maxKey;
+        Arrays.sort(nums);
+        return nums[nums.length/2]; //after sorting the max val will atleast cross n/2 position
     }
 }
