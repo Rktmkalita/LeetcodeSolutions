725053975ce9bfb702faf949dd772336991496b0
Raktim Kalita <rktmkalita2@gmail.com>
2022-12-22 21:46:13 +0530

    Time: 5 ms (94.86%), Space: 42.1 MB (92.02%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        for(int i = 0; i < s.length(); i++){
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+            if(s.lastIndexOf(c1) != t.lastIndexOf(c2))
+                return false;
+        }   
+        return true;
+    }
+}
