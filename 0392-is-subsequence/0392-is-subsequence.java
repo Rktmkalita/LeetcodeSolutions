f432ba42e2aab5dffdcf554eb38e5f5d97c7cbeb
Raktim Kalita <rktmkalita2@gmail.com>
2022-12-22 21:02:30 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (79.95%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if(s.length()>t.length())
+            return false;
+        int pos=0;
+        for(int i=0;i<s.length();i++){
+            int flag=0;
+            char sch=s.charAt(i);
+            for(int j=pos;j<t.length();j++){
+                char tch=t.charAt(j);
+                if(sch==tch){
+                    flag=1;
+                    pos=j+1;
+                    break;
+                }
+            }
+            if(flag==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
