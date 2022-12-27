c158c470fa16066f29d936a2ee2f2f530f70ad96
Raktim Kalita <rktmkalita2@gmail.com>
2022-12-27 21:16:33 +0530

    Time: 20 ms (71.70%), Space: 39 MB (89.42%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -5,19 +5,18 @@ public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int low=1;
         int high=n;
-        int bad=0;
+        int mid=0;
         while(low<=high){
-            int mid=low+(high-low)/2;
+            mid=low+(high-low)/2;
             if(isBadVersion(mid)){
                 if(!isBadVersion(mid-1)){
                     return mid;
                 }
-                // bad=(int)mid;
                 high=mid-1;
             }else{
                 low=mid+1;
             }
         }
-        return bad;
+        return mid;
     }
 }
