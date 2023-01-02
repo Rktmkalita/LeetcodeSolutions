a4c439524f6ecf6f5266c72390a2a8caa61ab5c4
Raktim Kalita <rktmkalita2@gmail.com>
2023-01-02 19:27:54 +0530

    Time: 3 ms (100.00%), Space: 43.7 MB (69.68%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,27 +1,63 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> list=new ArrayList<>();
-        if(p.length()>s.length())
-            return list;
-        int[] pcount=new int[26];
-        for(char ch:p.toCharArray()){
-            pcount[ch-'a']++;
+        if(s.length() < p.length()){
+            return new LinkedList<>();
         }
-        for(int i=0;i<=s.length()-p.length();i++){
-            int[] scount=new int[26];
-            boolean flag=true;
-            for(char ch:s.substring(i,p.length()+i).toCharArray()){
-                scount[ch-'a']++;
+
+        List<Integer> result = new LinkedList<>();
+        int left = 0;
+        int right = p.length() - 1;
+
+        int[] count = new int[26]; // hashmap: char to count
+        
+        for(char ch: p.toCharArray()){
+            int index = ch - 'a';
+            count[index]--;
+        }
+
+        for(int i=left; i<=right; i++){
+            char ch = s.charAt(i);
+            int index = ch - 'a';
+            count[index]++;
+        }
+
+        boolean good = true;
+
+        for(int value: count){
+            if(value != 0){
+                good = false;
+                break;
             }
-            for(int j=0;j<26;j++){
-                if(scount[j]!=pcount[j]){
-                    flag=false;
+        }
+
+        if(good){
+            result.add(left);
+        }
+       
+        while(right < s.length() - 1){
+            left++;
+            right++;
+            
+            char charOld = s.charAt(left-1);
+            char charNew = s.charAt(right);
+            
+            count[charOld - 'a']--;                  
+            count[charNew - 'a']++;
+
+            good = true;
+
+            for(int value: count){
+                if(value != 0){
+                    good = false;
                     break;
                 }
             }
-            if(flag==true)
-                list.add(i);
+        
+            if(good){
+                result.add(left);
+            }
         }
-        return list;
+
+        return result;
     }
 }
