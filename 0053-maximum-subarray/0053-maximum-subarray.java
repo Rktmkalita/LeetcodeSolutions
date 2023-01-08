8bd7cd43f9d5a37097feec27553d835525c84ddc
Raktim Kalita <rktmkalita2@gmail.com>
2023-01-08 21:54:11 +0530

    Time: 1 ms (100.00%), Space: 51.6 MB (85.35%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sumMax=nums[0];
+        int sum=sumMax;
+        for(int i=1;i<nums.length;i++){
+            if(sum<0)
+                sum=0;
+            sum+=nums[i];
+            if(sumMax<sum){
+                sumMax=sum;
+            }
+        }
+        
+        return sumMax;
+    }
+}
