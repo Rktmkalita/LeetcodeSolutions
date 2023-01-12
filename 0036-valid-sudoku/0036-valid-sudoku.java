0b1baa008700feb83270b118a8099c237f920f67
Raktim Kalita <rktmkalita2@gmail.com>
2023-01-12 21:22:21 +0530

    Time: 1 ms (100.00%), Space: 41.9 MB (98.28%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,14 +1,49 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Set<String> seen = new HashSet();
-        for (int i=0; i<9; ++i) {
-            for (int j=0; j<9; ++j) {
-                char number = board[i][j];
-                if (number != '.')
-                    if (!seen.add(number + " in row " + i) ||
-                        !seen.add(number + " in column " + j) ||
-                        !seen.add(number + " in block " + i/3 + "-" + j/3))
-                        return false;
+        // check rows
+        for (char[] row : board) {
+            if (!checkValid(row)) {
+                return false;
+            }
+        }
+        // check columns
+        for (int col = 0; col < board[0].length; col++) {
+            char[] column = new char[board.length];
+            for (int row = 0; row < board.length; row++) {
+                column[row] = board[row][col];
+            }
+            if (!checkValid(column)) {
+                return false;
+            }
+        }
+        // check 3x3 sub-grids
+        for (int i = 0; i < 9; i += 3) {
+            for (int j = 0; j < 9; j += 3) {
+                char[] grid = new char[9];
+                int k = 0;
+                for (int row = i; row < i + 3; row++) {
+                    for (int col = j; col < j + 3; col++) {
+                        grid[k++] = board[row][col];
+                    }
+                }
+                if (!checkValid(grid)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean checkValid(char[] lst) {
+        // check if list contains 1-9 without repetition
+        boolean[] seen = new boolean[9];
+        for (char c : lst) {
+            if (c != '.') {
+                int index = c - '1';
+                if (seen[index]) {
+                    return false;
+                }
+                seen[index] = true;
             }
         }
         return true;
