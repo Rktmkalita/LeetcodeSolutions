98a2e0daa5f0f1cade25bbcc7c83719855599942
Raktim Kalita <rktmkalita2@gmail.com>
2023-01-13 21:03:39 +0530

    Time: 2 ms (97.16%), Space: 42.4 MB (85.02%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,5 +1,7 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if (s == null || t == null)
+            return false;
         if(s.length()!=t.length())
             return false;
         int[] counter = new int[26];
