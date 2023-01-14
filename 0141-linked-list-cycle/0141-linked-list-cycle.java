e3cb586ec333cc913abb3e83e2ebc52b2336a816
Raktim Kalita <rktmkalita2@gmail.com>
2023-01-14 20:37:11 +0530

    Time: 0 ms (100.00%), Space: 43.8 MB (57.56%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -15,13 +15,11 @@ public class Solution {
             return false;
         }
         ListNode slow_p = head, fast_p = head;
-        int flag = 0;
         while (slow_p != null && fast_p != null
                && fast_p.next != null) {
             slow_p = slow_p.next;
             fast_p = fast_p.next.next;
             if (slow_p == fast_p) {
-                flag = 1;
                 return true;
             }
         }
