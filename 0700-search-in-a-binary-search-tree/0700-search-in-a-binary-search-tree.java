003b6abf1cf1cfdb5610f82bf169aa69fc139e06
Raktim Kalita <rktmkalita2@gmail.com>
2023-01-20 20:37:17 +0530

    Time: 0 ms (100.00%), Space: 43 MB (50.14%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
@@ -15,26 +15,16 @@
  */
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        if(root==null){
+        if (root == null) {
             return null;
         }
-        return helper(root,val);
-    }
-    public TreeNode helper(TreeNode root, int val){
-        if(root==null){
-            return null;
-        }
-        if(root.val==val){
+        if (root.val == val) {
             return root;
         }
-        TreeNode left=helper(root.left,val);
-        TreeNode right=helper(root.right,val);
-        if(left!=null){
-            return left;
-        }else if(right!=null){
-            return right;
-        }else{
-            return null;
+        if (root.val > val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
         }
     }
 }
