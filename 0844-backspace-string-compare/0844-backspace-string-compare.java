9f425cf5afb5ef96b35783b85994284b4f2768f6
Raktim Kalita <rktmkalita2@gmail.com>
2023-01-21 21:11:11 +0530

    Time: 15 ms (5.67%), Space: 43.8 MB (6.59%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        if(helper(s).equals(helper(t)))
+            return true;
+        return false;
+    }
+    private String helper(String s){
+        char[] chArr = s.toCharArray();
+        String str="";
+        for(char ch:chArr){
+            if(ch=='#' && str.length()>0){
+                str=str.substring(0,str.length()-1);
+            }else if(ch=='#'){
+                continue;
+            }else{
+                str+=ch;
+            }
+        }
+        System.out.println(str);
+        return str;
+    }
+}
