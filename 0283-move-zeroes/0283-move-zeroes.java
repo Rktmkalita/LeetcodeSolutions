db34af5454a47b5f87662110c0e703eccfcd6e55
Raktim Kalita <rktmkalita2@gmail.com>
2023-01-25 21:10:08 +0530

    Time: 3 ms (43.19%), Space: 55.1 MB (11.25%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,27 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        int j=1;
         for(int i=0;i<nums.length-1;i++){
             if(nums[i]==0){
-                for(int j=i+1;j<nums.length;j++){
+                while(j<nums.length){
                     if(nums[j]!=0){
                         int temp = nums[j];
                         nums[j]=nums[i];
                         nums[i]=temp;
                         break;
                     }
+                    j++;
                 }
+                // for(int j=i+1;j<nums.length;j++){
+                //     if(nums[j]!=0){
+                //         int temp = nums[j];
+                //         nums[j]=nums[i];
+                //         nums[i]=temp;
+                //         break;
+                //     }
+                // }
+            }else{
+                j++;
             }
         }
     }
