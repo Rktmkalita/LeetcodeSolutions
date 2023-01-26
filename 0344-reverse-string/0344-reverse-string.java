ef5e20cee31fc71c6a2b234a0d5e3e5c48a05625
Raktim Kalita <rktmkalita2@gmail.com>
2023-01-26 20:10:36 +0530

    Time: 1 ms (98.90%), Space: 48.9 MB (90.68%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,15 +1,10 @@
 class Solution {
     public void reverseString(char[] s) {
-        int left = 0;
-        int right = s.length - 1;
-
-        while (left < right) {
-            char temp = s[left];
-            s[left] = s[right];
-            s[right] = temp;
-
-            left++;
-            right--;
+        int n=s.length;
+        for(int i=0;i<n/2;i++){
+            char temp=s[i];
+            s[i]=s[n-1-i];
+            s[n-1-i]=temp;
         }
     }
 }
