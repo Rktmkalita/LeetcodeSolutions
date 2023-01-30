87c56211380caf91b1eefac8967d7ac7a7b13d6e
Raktim Kalita <rktmkalita2@gmail.com>
2023-01-30 21:20:24 +0530

    Time: 2 ms (100.00%), Space: 42.3 MB (86.96%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -11,14 +11,9 @@ class Solution {
         return maxCount;
     }
     public int dfs(int[][] grid, int row, int col){
-        // int count=0;
         if(row<0 || col<0 || row>=grid.length || col>=grid[0].length || grid[row][col]!=1){
             return 0;
         }
-        // if(grid[row][col]==1){
-        //     grid[row][col]=0;
-        //     count+=dfs(grid,row-1,col)+dfs(grid,row+1,col)+dfs(grid,row,col-1)+dfs(grid,row,col+1)+1;
-        // }
         grid[row][col]=0;
         return dfs(grid,row-1,col)+dfs(grid,row+1,col)+dfs(grid,row,col-1)+dfs(grid,row,col+1)+1;
     }
