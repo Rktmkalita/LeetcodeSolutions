c8c1f88cab4f08f085e9c679d2cf598ef9c6c773
Raktim Kalita <rktmkalita2@gmail.com>
2023-02-05 20:43:42 +0530

    Time: 4 ms (52.66%), Space: 43.2 MB (32.46%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,9 +1,23 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int sum = 0;
-        for (var num: nums) {
-            sum = sum ^ num;
+        int[] count = new int[60001];
+        for(int i:nums){
+            if(i<0){
+                count[-i+30000]++;
+            }else
+                count[i]++;
         }
-        return sum;  
+        int j=0;
+        for(int i:count){
+            if(i==1){
+                if(j>30000){
+                    j=j-30000;
+                    j=-j;
+                }
+                return j;
+            }
+            j++;
+        }
+        return 0;
     }
 }
