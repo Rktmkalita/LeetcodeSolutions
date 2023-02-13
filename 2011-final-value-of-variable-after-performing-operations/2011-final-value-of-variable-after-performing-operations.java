7d78e4049140f900fdf49e04f29d5bb6bedf4ba7
Raktim Kalita <rktmkalita2@gmail.com>
2023-02-13 22:29:03 +0530

    Time: 1 ms (82.31%), Space: 42 MB (43.18%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int count=0;
+        for(int i=0;i<operations.length;i++){
+            if(operations[i].equals("X++") || operations[i].equals("++X"))
+                count++;
+            else
+                count--;
+        }
+        return count;
+    }
+}
