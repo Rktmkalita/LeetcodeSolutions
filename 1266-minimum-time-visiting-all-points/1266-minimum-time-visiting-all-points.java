e360ba44a14a59e4ed58fe0d761d0038093f8003
Raktim Kalita <rktmkalita2@gmail.com>
2023-02-24 21:52:18 +0530

    Time: 1 ms (82.76%), Space: 41.6 MB (89.05%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.java
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.java
@@ -6,13 +6,14 @@ class Solution {
             int x2=points[i][0];
             int y1=points[i-1][1];
             int y2=points[i][1];
-            if(x1==x2 && y2!=y1){
-                time+=Math.abs(y2-y1);
-            }else if(x2!=x1 && y2==y1){
-                time+=Math.abs(x2-x1);
-            }else if(x2!=x1 && y2!=y1){
-                time+=Math.max(Math.abs(x2-x1),Math.abs(y2-y1));
-            }
+            // if(x1==x2 && y2!=y1){
+            //     time+=Math.abs(y2-y1);
+            // }else if(x2!=x1 && y2==y1){
+            //     time+=Math.abs(x2-x1);
+            // }else if(x2!=x1 && y2!=y1){
+            //     time+=Math.max(Math.abs(x2-x1),Math.abs(y2-y1));
+            // }
+            time+=Math.max(Math.abs(x2-x1),Math.abs(y2-y1));
         }
         return time;
     }
