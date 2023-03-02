c69a9c97e7029023d0c6fa6ba75ddf0e28f5a37c
Raktim Kalita <rktmkalita2@gmail.com>
2023-03-02 21:03:42 +0530

    Time: 1 ms (89.61%), Space: 41.2 MB (41.64%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
@@ -4,14 +4,7 @@ class Solution {
         for(int i=0;i<=s.length()-3;i++){
             boolean flag=true;
             String str = s.substring(i,i+3);
-            for(int j=0;j<2;j++){
-                char ch=str.charAt(j);
-                if(str.lastIndexOf(ch)!=j){
-                    flag=false;
-                    break;
-                }
-            }
-            if(flag)
+            if(str.lastIndexOf(str.charAt(0))==0 && str.lastIndexOf(str.charAt(1))==1)
                 count++;
         }
         return count;
