046d9039ec2f43876d8814323a82400ec35c0521
Raktim Kalita <rktmkalita2@gmail.com>
2023-03-02 21:04:00 +0530

    Time: 1 ms (89.61%), Space: 40.8 MB (48.45%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
@@ -2,7 +2,6 @@ class Solution {
     public int countGoodSubstrings(String s) {
         int count=0;
         for(int i=0;i<=s.length()-3;i++){
-            boolean flag=true;
             String str = s.substring(i,i+3);
             if(str.lastIndexOf(str.charAt(0))==0 && str.lastIndexOf(str.charAt(1))==1)
                 count++;
