9127f0ea23dae08656f3eb9f6ff3ec496d174b44
Raktim Kalita <rktmkalita2@gmail.com>
2023-05-20 21:20:48 +0530

    Time: 2020 ms (29.54%), Space: 0B (100.00%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,6 +1,7 @@
 # Write your MySQL query statement below
 select temp.Department, temp.Employee, temp.Salary from
-(select d.name as Department, e.name as Employee, e.Salary, dense_rank() over(partition by e.departmentId order by e.salary desc) as ranking
+(select d.name as Department, e.name as Employee, e.Salary,
+dense_rank() over(partition by e.departmentId order by e.salary desc) as ranking
 from Employee e join Department d on e.departmentId = d.id) as temp
 where ranking<=3;
 
